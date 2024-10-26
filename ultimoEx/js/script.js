17bc780a07833401386bbc1b9d84d7fd32049bdc
Luan Messias <luanmesias1211@gmail.com>
2024-10-26 11:31:09 -0300

    att

--- ultimoEx/js/script.js
+++ ultimoEx/js/script.js
@@ -1,13 +1,13 @@
 function meuEscopo () {
 const form = document.querySelector('.form');
+
 let contador = 1;
 function recebeEventoForm (evento) {
     evento.preventDefault();
-    console.log(`Form não enviado ${contador}`)
-    contador++
+    console.log(`Form não enviado ${contador}`);
+    contador++;
     }
 
 form.addEventListener('submit', recebeEventoForm);
-
 }
 meuEscopo ();
